09baeecd56097ae69af6a82b2fb7f176de886e70
er-rahulnikam <errahulnikam@gmail.com>
2024-12-22 09:53:23 +0530

    Project Running fine but there is error when user not provided email or password for login

--- ShoppingCart/src/main/java/com/ecom/util/CommonUtil.java
+++ ShoppingCart/src/main/java/com/ecom/util/CommonUtil.java
@@ -1,46 +1,47 @@
 package com.ecom.util;
 
-//import java.io.UnsupportedEncodingException;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.mail.javamail.JavaMailSender;
-//import org.springframework.mail.javamail.MimeMessageHelper;
-//import org.springframework.stereotype.Component;
-//
-//import jakarta.mail.MessagingException;
-//import jakarta.mail.internet.MimeMessage;
-//import jakarta.servlet.http.HttpServletRequest;
-//
-//@Component
+import java.io.UnsupportedEncodingException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import jakarta.servlet.http.HttpServletRequest;
+
+@Component
 public class CommonUtil {
 
-//	@Autowired
-//	private JavaMailSender mailSender;
-//
-//	public Boolean sendMail(String url, String reciepentEmail) throws UnsupportedEncodingException, MessagingException {
-//
-//		MimeMessage message = mailSender.createMimeMessage();
-//		MimeMessageHelper helper = new MimeMessageHelper(message);
-//
-//		helper.setFrom("daspabitra55@gmail.com", "Shooping Cart");
-//		helper.setTo(reciepentEmail);
-//
-//		String content = "<p>Hello,</p>" + "<p>You have requested to reset your password.</p>"
-//				+ "<p>Click the link below to change your password:</p>" + "<p><a href=\"" + url
-//				+ "\">Change my password</a></p>";
-//		helper.setSubject("Password Reset");
-//		helper.setText(content, true);
+	@Autowired
+	private JavaMailSender mailSender;
+
+	public Boolean sendMail(String url, String reciepentEmail) throws UnsupportedEncodingException, MessagingException {
+
+		MimeMessage message = mailSender.createMimeMessage();
+		MimeMessageHelper helper = new MimeMessageHelper(message);
+
+		helper.setFrom("xyz@gmail.com", "Shooping Cart");
+		helper.setTo(reciepentEmail);
+
+		String content = "<p>Hello,</p>" + "<p>You have requested to reset your password.</p>"
+				+ "<p>Click the link below to change your password:</p>" + "<p><a href=\"" + url
+				+ "\">Change my password</a></p>";
+		helper.setSubject("Password Reset");
+		helper.setText(content, true);
 //		mailSender.send(message);
-//		return true;
-//	}
-//
-//	public static String generateUrl(HttpServletRequest request) {
-//
-//		// http://localhost:8080/forgot-password
-//		String siteUrl = request.getRequestURL().toString();
-//
-//		return siteUrl.replace(request.getServletPath(), "");
-//
-//	}
+		System.err.println(url);
+		return true;
+	}
+
+	public static String generateUrl(HttpServletRequest request) {
+
+		// http://localhost:8080/forgot-password
+		String siteUrl = request.getRequestURL().toString();
+
+		return siteUrl.replace(request.getServletPath(), "");
+
+	}
 
 }
