941d908d7b9b833b1205b3248036e26b1a851c5a
er-rahulnikam <errahulnikam@gmail.com>
2024-12-25 13:23:59 +0530

    saveUser stepes updated

--- ShoppingCart/src/main/java/com/ecom/service/UserService.java
+++ ShoppingCart/src/main/java/com/ecom/service/UserService.java
@@ -2,11 +2,13 @@ package com.ecom.service;
 
 import java.util.List;
 
+import org.springframework.web.multipart.MultipartFile;
+
 import com.ecom.model.UserDtls;
 
 public interface UserService {
 
-	public UserDtls saveUser(UserDtls user);
+	public Boolean saveUser(UserDtls user, MultipartFile file);
 
 	public UserDtls getUserByEmail(String email);
 
